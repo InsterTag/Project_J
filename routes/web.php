9b608853ce1942db45e03a3c44a3fdb6f2151a58
InsterTag <cajassandovalestiven01@gmail.com>
2025-04-20 19:40:55 -0500

    Se agregan los Mensajes

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,17 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/home', function () {
-    return view('home');
-})->name('home-route');
-
+//Rutas para la vista 
 Route::view('/login', 'login')->name('login');
 Route::view('/home', 'home')->middleware('auth')->name('home');
 
+//Rutas para crear un Usuario
 Route::get('/register',[UserController::class,'create'])->name('register');
 Route::post('/crearUsuario',[UserController::class,'agg_user'])->name('create-user');
 Route::post('/inicia-sesion',[UserController::class,'login'])->name('inicia-sesion');
 Route::get('/logout',[UserController::class,'logout'])->name('logout');
 
 
-
-
+Route::get('/message/create', [MessageController::class, 'create'])->name('message-form');
+Route::post('/message/send', [MessageController::class, 'send_message'])->name('send-message');
+Route::get('/messages', [MessageController::class, 'index'])->name('messages')->middleware('auth');
